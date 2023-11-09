f6daf420827a33ac2fee82a0fd820d1b9d5ac4e0
ShrkeyAMB <mymshrkeymym@gmail.com>
2023-11-09 11:45:16 -0800

    LessonD

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,12 +4,38 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject powerUpPrefab;
     public GameObject enemyPrefab;
     private float spawnRange = 8.5f;
+    private int enemyCount;
+
+    private int waveNumber = 1;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnWave(3);
+        
+    }
+
+    private void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        {
+            if(enemyCount == 0)
+            {
+                waveNumber++;
+                SpawnWave(waveNumber);
+            }
+        }
+    }
+
+    void SpawnWave(int enemyNum)
+    {
+        Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
+        for (int i = 0; i < enemyNum; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 
     Vector3 GenerateSpawnPosition()
